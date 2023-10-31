a2ffe08a555948e40ff60d9f86782152b1f6d6fc
JacopoNicolosiAlten <jacopo.nicolosi@alten.it>
2023-10-31 11:01:03 +0100

    create filety/categories/spec folder for customization

--- filety/categories/__init__.py
+++ filety/categories/__init__.py
@@ -2,10 +2,10 @@ import glob, os
 from importlib import import_module
 
 file_categories_map = dict()
-_relative_path = os.path.join('custom_package', 'filety', 'categories')
+_relative_path = os.path.join('custom_package', 'filety', 'categories', 'spec')
 for category_path in glob.glob(os.path.join(_relative_path, '**.py'), recursive=True):
     basename = os.path.basename(category_path)
-    if basename not in {'__init__.py', 'cateory_template.py', 'utils.py'}:
+    if basename != 'utils.py':
         category = basename.split('.')[0]
         module = import_module('.' + category, _relative_path.replace(os.path.sep, '.'))
         file_categories_map[category] = getattr(module, 'map')
