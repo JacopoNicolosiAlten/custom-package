1e4396347073950fd5519d8c5b795b97b1ad1c3c
JacopoNicolosiAlten <jacopo.nicolosi@alten.it>
2023-10-31 11:27:49 +0100

    move categories_specifications outside the directory

--- filety/categories/__init__.py
+++ filety/categories/__init__.py
@@ -2,7 +2,7 @@ import glob, os
 from importlib import import_module
 
 file_categories_map = dict()
-_relative_path = os.path.join('custom_package', 'filety', 'categories', 'spec')
+_relative_path = 'categories_specifications'
 for category_path in glob.glob(os.path.join(_relative_path, '**.py'), recursive=True):
     basename = os.path.basename(category_path)
     if basename != 'utils.py':
